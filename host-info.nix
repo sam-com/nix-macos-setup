41d9be50e082d58c5d2ce114169717dbe9a6b921
Samuel Comeau <24446727+sam-com@users.noreply.github.com>
2026-02-24 12:24:45 -0500

    Comment out git configuration in host-info.nix
    Commented out git username and email fields in host-info.nix.

--- host-info.nix
+++ host-info.nix
@@ -1,6 +1,10 @@
 # Auto-generated host information for Nix flake
 # This file is created by install.sh
 {
+  # git = {
+  #   username = "NAME";
+  #   email = "EMAIL";
+  # };
   hostname = "HOSTNAME";
   username = "USERNAME";
   homedir = "HOMEDIR";
